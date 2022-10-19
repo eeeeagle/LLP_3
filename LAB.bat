c6f4352c4d0db65002d76aad5ea8bdfd8080a826
Daniil Osokin <90867530+eeeeagle@users.noreply.github.com>
2022-10-19 13:48:14 +0400

    .

---
+++ LAB.bat
@@ -0,0 +1,14 @@
+@ECHO TASK 1 - EASY
+Lab\Task1\Easy\easy.exe < Lab\Task1\Easy\easy.txt
+@ pause
+cls
+
+@ECHO TASK 1 - HARD
+Lab\Task1\Hard\hard.exe < Lab\Task1\Hard\hard.txt
+@ pause
+cls
+
+@ECHO TASK 2
+Lab\Task2\main.exe
+@ pause
+cls
